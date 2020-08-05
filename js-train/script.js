4bcec6f6f7ddc4f8188acddad81aa4bd9f91b95e
Mick430 <michael.chamoun.bee@outlook.com>
2020-08-05 09:17:48 +0200

    js scipt test

---
+++ js-train/script.js
@@ -0,0 +1,10 @@
+// getElementById
+// document.getElementById("demo").innerHTML = "testing";
+
+// document.write
+// document.getElementsByTagName("button")
+document.write(8+9);
+
+window.alert(5+7);
+
+console.log("test");
